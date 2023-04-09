b4c14296c8fe5872f745752eafb14dfd2f94b99d
Bizzy-coding <bizzycoding@gmail.com>
2023-04-09 19:39:31 +0100

    sections and styles updated

---
+++ src/Components/Contact.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+export default function Contact() {
+    return (
+
+        <section className="contact">
+            <div className="inner-wrap">
+            <h3>Still have questions</h3>
+            <p>Feel free to contact any time</p>
+            </div>
+            <a class="contact-btn" href="mailto:bizzy_coding@gmail.com">Contact Us</a>
+           
+
+        </section>
+        
+    )
+}
---
+++ src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+export default function Header() {
+    return (
+        <header>
+            <p>Frequently Asked Questions</p>
+            <h1>We're here to help</h1>
+            <p>Got questions, no problem. Check out our FAQs or contact us below.</p>
+            <a class="contact-btn" href="mailto:bizzy_coding@gmail.com">Contact Us</a>
+        </header>
+    )
+}
